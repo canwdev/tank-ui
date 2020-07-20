92033962fc7a93a99c68d818043085fd7b03cee8
canwdev <c2097@live.com>
2020-07-20 22:34:22 +0800

    add demo components

--- docs/main.js
+++ docs/main.js
@@ -3,8 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import TankUI from '../src/index'
 
 Vue.config.productionTip = false
+
+Vue.use(TankUI)
 
 /* eslint-disable no-new */
 new Vue({
