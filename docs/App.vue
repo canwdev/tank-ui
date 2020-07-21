8091c7d26065b451a83af41939160b0b11e2ab8d
kenny <c2097@live.com>
2020-07-21 20:16:06 +0800

    add Container, Link

--- docs/App.vue
+++ docs/App.vue
@@ -2,19 +2,35 @@
   <div id="app">
     <TkNavHeader
       :menu-list="[
-        {title: '首页', url: '/'},
-        {title: '文档', url: '/'},
-        {title: '资源', url: '/'}
+        {title: '入门', url: '/quickstart'},
+        {title: '文档', url: '/docs'},
+        {title: '资源', url: '', children: [
+          {title: 'Github', url: 'https://github.com/fe-player/tank-ui'},
+          {title: 'Gitee', url: 'https://gitee.com/fe-player/tank-ui'}
+        ]}
       ]"
-    />
+    >
+      <template slot="headerLeft">
+        <NavLogo></NavLogo>
+      </template>
+      <template slot="headerRight">
+        <TkLink>搜索</TkLink>
+      </template>
+    </TkNavHeader>
+
     <router-view/>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  import NavLogo from '@/components/NavLogo'
+
+  export default {
+    name: 'App',
+    components: {
+      NavLogo
+    }
+  }
 </script>
 
 <style>
