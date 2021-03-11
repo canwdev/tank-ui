59d12dcb17a8124c456b76a25822ef2ef61dc0ed
kenny <c2097@live.com>
2021-03-11 15:01:49 +0800

    convert

--- docs/components/NavLogo.vue
+++ docs/components/NavLogo.vue
@@ -6,42 +6,47 @@
 </template>
 
 <script>
-  export default {
-    name: 'NavLogo'
-  }
+export default {
+  name: 'NavLogo'
+}
 </script>
 
-<style lang="stylus" scoped>
-  $nav_height = 50px
+<style lang="scss" scoped>
+$nav_height: 50px;
+.nav-logo {
+  display: flex;
+  align-items: center;
+  text-decoration: none !important;
+  color: $color_black;
 
-  .nav-logo
-    display flex
-    align-items center
-    text-decoration: none !important;
-    color $color_black
+  &:hover {
+    opacity: 0.9;
+  }
 
-    &:hover
-      opacity 0.9
+  &:active {
+    opacity: 1;
+  }
 
-    &:active
-      opacity: 1;
+  & > img {
+    width: $nav_height;
+    height: $nav_height;
+    border-radius: 50%;
+    transition: all 0.25s;
+    transition-timing-function: $transition_fx2;
+    @media screen and (max-width: $mq_mobile_width) {
+      width: $nav_height - 12px;
+      height: $nav_height - 12px;
+    }
+  }
 
-    & > img
-      width $nav_height
-      height $nav_height
-      border-radius 50%
-      transition all .25s
-      transition-timing-function $transition_fx2
-      @media screen and (max-width $mq_mobile)
-        width ($nav_height - 12px)
-        height ($nav_height - 12px)
-
-    & > p
-      font-size 18px
-      font-weight: bold
-      margin-left: 10px
-      transition font-size .25s
-      @media screen and (max-width $mq_mobile)
-        font-size 18px
-
+  & > p {
+    font-size: 18px;
+    font-weight: bold;
+    margin-left: 10px;
+    transition: font-size 0.25s;
+    @media screen and (max-width: $mq_mobile_width) {
+      font-size: 18px;
+    }
+  }
+}
 </style>
